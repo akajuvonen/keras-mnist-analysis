ff171c0327caf9adb9ade39f99cdb70191e46091
Antti Juvonen <akajuvonen@users.noreply.github.com>
2018-01-05 19:22:14 +0200

    Added a note

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 from keras.datasets import mnist
 
 # Load data, training and testing sets
+# The data is already shuffled at this point
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+print(max(x_train.any()))
