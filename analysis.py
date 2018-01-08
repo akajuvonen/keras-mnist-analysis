24a851457a60092d650c8aa45117e6f05cb2d9d6
Antti Juvonen <akajuvonen@users.noreply.github.com>
2018-01-08 11:55:26 +0200

    Added the first convolutional layer

--- analysis.py
+++ analysis.py
@@ -1,7 +1,11 @@
 from keras.utils import to_categorical
 from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 
 N_CLASSES = 10
+INPUT_SHAPE = (28, 28, 1)
+EPOCHS = 10
 
 # Load data, training and testing sets
 # The data is already shuffled at this point
@@ -18,3 +22,8 @@ x_test = x_test / 255
 # Perform one-hot encoding for output
 y_train = to_categorical(y_train, N_CLASSES)
 y_test = to_categorical(y_test, N_CLASSES)
+
+# We use the sequential model
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size=(3, 3), activation='relu',
+                 input_shape=INPUT_SHAPE))
