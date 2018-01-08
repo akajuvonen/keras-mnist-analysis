14dccb7f1f4c48bd94549480b5518e5ced59a9f5
Antti Juvonen <akajuvonen@users.noreply.github.com>
2018-01-08 12:00:38 +0200

    Completed the first simple version of the conv net architecture

--- analysis.py
+++ analysis.py
@@ -25,5 +25,12 @@ y_test = to_categorical(y_test, N_CLASSES)
 
 # We use the sequential model
 model = Sequential()
+# A 2-D convolutional layer
 model.add(Conv2D(filters=16, kernel_size=(3, 3), activation='relu',
                  input_shape=INPUT_SHAPE))
+# Pooling layer with pool size and strides = (2,2)
+model.add(MaxPooling2D(pool_size=2))
+# Flattens the output for normal neural network layer
+model.add(Flatten())
+# Output layer with 10 outputs, uses softmax classifier
+model.add(Dense(10, activation='softmax'))
