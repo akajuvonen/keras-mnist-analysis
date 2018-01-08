25c7eee4a10ae10c7308a92836e4d1051f1e914e
Antti Juvonen <akajuvonen@users.noreply.github.com>
2018-01-08 12:43:08 +0200

    Added confusion matrix printing

--- analysis.py
+++ analysis.py
@@ -4,10 +4,12 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from keras.losses import categorical_crossentropy
 from keras.optimizers import Adam
+from sklearn.metrics import confusion_matrix
+import numpy as np
 
 N_CLASSES = 10
 INPUT_SHAPE = (28, 28, 1)
-EPOCHS = 3
+EPOCHS = 10
 BATCH_SIZE = 100
 
 # Load data, training and testing sets
@@ -54,3 +56,10 @@ model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS,
 loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
 print("Loss: ", loss)
 print("Accuracy: ", accuracy)
+
+# Print confusion matrix
+y_predicted = model.predict(x_test)
+y_predicted = np.argmax(y_predicted, axis=1)
+y_actual = np.argmax(y_test, axis=1)
+cm = confusion_matrix(y_actual, y_predicted)
+print(cm)
